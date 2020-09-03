6fc16785a3888242cde86caba8ac1a9d59658cf0
vakas-786 <vakas0959@gmail.com>
2020-09-03 16:14:25 -0400

    basic routes

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Navbar from './Components/Navbar'
+import Login from './Components/Login'
+import About from './Components/About'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+    <Router>
+        <div>
+        <Navbar />
+        <Route exact path="/" component={App} />
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/About" component={About} />
+        </div>
+
+    </Router>),
+        document.getElementById('root')
+)
+
